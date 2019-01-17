101979bdd63684c30b97462d5f1105d91a6520b0
Andrew Shi <andrew@syntechcorporation.com>
2019-01-16 17:16:19 -0800

    Change version number to be PEP compatible

--- autoflake.py
+++ autoflake.py
@@ -43,7 +43,7 @@ import pyflakes.messages
 import pyflakes.reporter
 
 
-__version__ = '1.3-munkyshi'
+__version__ = '1.3.dev0'
 
 
 ATOMS = frozenset([tokenize.NAME, tokenize.NUMBER, tokenize.STRING])
